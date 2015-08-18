3559327cee0951916d74bc863b39f4f4a3962439
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-18 14:42:43 -0700

    Serve up JSON string of Qs

--- server/models/trivia/triviaRoutes.js
+++ server/models/trivia/triviaRoutes.js
@@ -1,5 +1,6 @@
 var triviaController = require('./triviaController.js');
 var unirest = require('unirest');
+var data = require('./triviaData');
 
 var cleanAnswer = function(answer) {
   return answer.replace(/<\/?i>/g, '');
@@ -7,25 +8,29 @@ var cleanAnswer = function(answer) {
 
 module.exports = function(app){
   app.post('/', triviaController.checkAnswer);
-  app.get('/', function(req, res) {
-    unirest.get("http://jservice.io/api/random?count=100") // changed to 100
-    .header("Accept", "application/json")
-    .end(function (result) {
-      var pureQuestionsArr = [];
-      triviaController.addQuestion(result);
-      for(var i = 0; i < result.body.length; i++){
-        var questionObj = result.body[i];
-        var answer = questionObj.answer;
-        questionObj.clue = triviaController.getClue(cleanAnswer(questionObj.answer));
-        if (/[^a-z]/i.test(answer) || answer === '') { // ^a-z means NOT a letter
-          delete questionObj.answer;
-        } else {
-          delete questionObj.answer;
-          pureQuestionsArr.push(questionObj);
-        }
-      }
-      res.send(pureQuestionsArr);
-    });
+  app.get('/', function(req, res){
+    res.send(data);
   });
-
+  
+//  app.get('/', function(req, res) {
+//    unirest.get("http://jservice.io/api/random?count=100") // changed to 100
+//    .header("Accept", "application/json")
+//    .end(function (result) {
+//      var pureQuestionsArr = [];
+//      triviaController.addQuestion(result);
+//      for(var i = 0; i < result.body.length; i++){
+//        var questionObj = result.body[i];
+//        var answer = questionObj.answer;
+//        questionObj.clue = triviaController.getClue(cleanAnswer(questionObj.answer));
+//        if (/[^a-z]/i.test(answer) || answer === '') { // ^a-z means NOT a letter
+//          delete questionObj.answer;
+//        } else {
+//          delete questionObj.answer;
+//          pureQuestionsArr.push(questionObj);
+//        }
+//      }
+//      res.send(pureQuestionsArr);
+//    });
+//  });
+//
 };
