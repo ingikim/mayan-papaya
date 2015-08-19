dbdf260f07260d2db7eb70017b6fca661d0dbf0b
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-19 10:46:13 -0700

    Removed old action from GET api/trivia, now sends all questions in array

--- server/models/trivia/triviaRoutes.js
+++ server/models/trivia/triviaRoutes.js
@@ -1,6 +1,6 @@
 var triviaController = require('./triviaController.js');
 var unirest = require('unirest');
-var data = require('./triviaData');
+var Trivia = require('./triviaModel');
 
 var cleanAnswer = function(answer) {
   return answer.replace(/<\/?i>/g, '');
@@ -9,28 +9,12 @@ var cleanAnswer = function(answer) {
 module.exports = function(app){
   app.post('/', triviaController.checkAnswer);
   app.get('/', function(req, res){
-    res.send(data);
+    Trivia.find(function(err, questions) {
+      if(err) {
+        res.sendStatus(500);
+      } else {
+        res.send(JSON.stringify(questions));
+      }    
+    });
   });
-  
-//  app.get('/', function(req, res) {
-//    unirest.get("http://jservice.io/api/random?count=100") // changed to 100
-//    .header("Accept", "application/json")
-//    .end(function (result) {
-//      var pureQuestionsArr = [];
-//      triviaController.addQuestion(result);
-//      for(var i = 0; i < result.body.length; i++){
-//        var questionObj = result.body[i];
-//        var answer = questionObj.answer;
-//        questionObj.clue = triviaController.getClue(cleanAnswer(questionObj.answer));
-//        if (/[^a-z]/i.test(answer) || answer === '') { // ^a-z means NOT a letter
-//          delete questionObj.answer;
-//        } else {
-//          delete questionObj.answer;
-//          pureQuestionsArr.push(questionObj);
-//        }
-//      }
-//      res.send(pureQuestionsArr);
-//    });
-//  });
-//
 };
