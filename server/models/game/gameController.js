eb727ad34ca50f795b4e259755d1e514ac5cbe8d
Emily Eng <emily.w.eng@gmail.com>
2015-08-19 11:51:29 -0700

    Get userlist when users join. Start Game button and event.

--- server/models/game/gameController.js
+++ server/models/game/gameController.js
@@ -1,18 +1,40 @@
-var connection = require('../../config/connection');
+// var connection = require('../../config/connection');
+var app = require('../../../app');
 var activeCodes = {};
 
 module.exports = {
+
+  startConnection: function(code) {
+    console.log("connection: app is " + app);
+    console.log("connection: app.app is " + app.app);
+    console.log("connection: app.io is " + app.io);
+
+    var namespace = app.io.of('/' + code);
+    namespace.on('connection', function(socket) {
+      console.log("connection: startConnection " + code);
+      socket.on('newuser', function(username) {
+        // store list of usernames here?
+        activeCodes[code].push(username);
+        namespace.emit('userlist', activeCodes[code]);
+      });
+      socket.on('startgame', function() {
+        console.log("Socket: startgame");
+        namespace.emit('startgame');
+      });
+    }); 
+  },
 
   // Generate a random code for a game "room" / socket namespace.
   newGame: function(req, res) {
     var code = Math.floor(Math.random() * 1000);
 
     // Try storing a list of the usernames for this game
-    activeCodes[code] = [req.user.username];
+    // activeCodes[code] = [req.user.username];
+    activeCodes[code] = [];
     console.log("newGame: user list for " + code + " is " + activeCodes[code]);
 
     // Initiate socket.io connection
-    connection.startConnection(code);
+    module.exports.startConnection(code);
 
     res.json({code: code});
     // TODO? - setup additional game state?
@@ -24,9 +46,9 @@ module.exports = {
     console.log("gameController: joinGame req for " + code);
 
     if (activeCodes[code]) {
-      activeCodes[code].push(req.user.username);
+      // activeCodes[code].push(req.user.username);
       console.log("joinGame: user list for " + code + " is " + activeCodes[code]);
-      connection.emitUserList(code, activeCodes[code]);
+      // connection.emitUserList(code, activeCodes[code]);
       res.send(200);
     } else {
       res.send(404);
