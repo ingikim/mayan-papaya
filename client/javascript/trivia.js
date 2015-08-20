01b541d6f4121704ffac9c28eec862c6e4904157
Emily Eng <emily.w.eng@gmail.com>
2015-08-19 20:21:30 -0700

    Began cleaning up the styling.

--- client/javascript/trivia.js
+++ client/javascript/trivia.js
@@ -89,6 +89,8 @@
           answered: $scope.answered
 
         });
+        // calculate who won for display in the endgame view
+        $scope.highScore();
         $location.path("/trivia/endgame"); // render endgame view
       }
     };
@@ -132,7 +134,8 @@
         $scope.gameTimer = undefined;
       }
       //initialize timer number
-      $scope.counter = 15;
+      // $scope.counter = 15;
+      $scope.counter = 1000;
       //countdown
       $scope.gameTimer = $interval(function() {
         $scope.counter--;
@@ -236,6 +239,22 @@
         $location.path("/trivia/play"); // render play view
         console.log("$location.path: " + $location.path());
       });
+    };
+
+    $scope.highScore = function() {
+      var currHighest;
+
+      console.log("Calculating highScore");
+
+      for (var key in $scope.userScores) {
+        if (currHighest === undefined) {
+          currHighest = [key, $scope.userScores[key]];
+        } else if ($scope.userScores[key] > currHighest[1]) {
+          currHighest = [key, $scope.userScores[key]];
+        }
+      }
+
+      $scope.winner = currHighest;
     };
 
   }]);
